ca632cc592e97efc700d9c1ea8c7ff944eed8c8e
riceCk <876496731@qq.com>
2019-02-13 18:52:05 +0800

    1231

---
+++ dist/precache-manifest.8913f95c81ca7bba5aa2c26090fbaac6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f4d88b6ea6bef59d06ae",
+    "url": "/js/chunk-vendors.ea01b174.js"
+  },
+  {
+    "revision": "8324b321441392598756",
+    "url": "/js/app.d1241e98.js"
+  },
+  {
+    "revision": "46d4ac1d54b2338d76bb74c29a1b5c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "f4d88b6ea6bef59d06ae",
+    "url": "/css/chunk-vendors.0bb9788c.css"
+  },
+  {
+    "revision": "8324b321441392598756",
+    "url": "/css/app.2deafa89.css"
+  }
+];
